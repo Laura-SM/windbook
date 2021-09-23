aa054d24dd27a50439a3a1b8e8f221716cd36f89
Laura <laura.sabater.musach@gmail.com>
2021-09-23 10:24:22 +0200

    Install dependencies: cors dotenv express jest mongoose morgan nodemon eslint

---
+++ backend/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-underscore-dangle': 0,
+  },
+};
